ce52d03a041680ed0285131ab27d72d50d6d647b
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-09-05 16:22:08 +0600

    4 files modified, 1 added

--- src/app/built-in-directives/built-in-directives.component.ts
+++ src/app/built-in-directives/built-in-directives.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./built-in-directives.component.css']
 })
 export class BuiltInDirectivesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
