039d9f3fe5db5da6afefa6b4d622b2791ddd319d
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-09-07 12:21:52 +0600

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { BuiltInDirectivesComponent } from './built-in-directives/built-in-directives.component';
+import {MatTabsModule} from '@angular/material/tabs';
+
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, MatTabsModule],
   declarations: [AppComponent, HelloComponent, BuiltInDirectivesComponent],
   bootstrap: [AppComponent]
 })
